8714d6078e0a9c2d058f16955c9e2c02af2122dd
tzaf h <tzaf24@gmail.com>
2023-08-26 18:57:42 +0300

    start styling the app

--- src/component/homepage.component.jsx
+++ src/component/homepage.component.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './homepage.styles.scss'
 import Dirctrory from './directory/directory.component';
-
-
+// import logo from '../../logo/png/logo-no-background.png'
+import logo from '../logo-no-background.png'
 const HomePage =() =>(
 
     <div className='homepage'>
+      <img className='logo' src={logo} alt='logo'/>
       <Dirctrory />
     </div>
 
